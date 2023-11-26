2963920b945f5ecd3530b005f774f6fb143409e1
admin <admin@DESKTOP-GRRBCBG>
2023-11-26 20:50:51 +0400

    grgggg

---
+++ GakhoProject/Controllers/MemberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PagedList.Mvc;
+using PagedList;
+namespace GakhoProject.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
